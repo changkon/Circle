f3edd29d8de4a533d1b7266fdcf755658aa164ca
Chang Kon Han <chang_kon@hotmail.com>
2016-01-30 19:30:25 +1300

    create event code first class

---
+++ circleapp_Runtime/circleappService/DataObjects/Event.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Mobile.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace circleappService.DataObjects
+{
+    /// <summary>
+    /// Event domain class.
+    /// </summary>
+
+    public class Event : EntityData
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime date { get; set; }
+        public string location { get; set; }
+        public List<string> tags { get; set; }
+    }
+}
