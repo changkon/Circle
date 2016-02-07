cd2a3bfbd2c524db85076e608394c365a2819ef7
Wal8800 <johnlaw1234@hotmail.com>
2016-02-08 10:06:06 +1300

    can login and register through GUI

--- CircleApp/www/js/registerController.js
+++ CircleApp/www/js/registerController.js
@@ -1,29 +1,95 @@
 var myApp = angular.module('starter.controllers')
 
+myApp.service('registrationService', function() {
+  var newUser = {
+    email : '',
+    phoneNumber : '',
+    password: '',
+    name: '',
+    age: '',
+    gender: ''
+
+  };
+  return newUser;
+});
+
 myApp.controller('StartScreenCtrl', function($scope, $http, $rootScope, $location){
+    $scope.user = {};
 
     $scope.startRegistration = function() {
         $location.path('/registration_require');
     }
+
+    $scope.login = function() {
+        console.log("LOGIN user: " + $scope.user.email + " - PW: " + $scope.user.password);
+        $http({
+            method: 'POST',
+            url: "https://circleapp.azurewebsites.net/api/auth",
+            data: {username: $scope.user.email, password: $scope.user.password},
+            headers: {'Content-Type': 'application/json'}
+        })
+        .success(function(response) {
+            // handle success things
+            console.log(response.token);
+            $rootScope.userId = response.id;
+            $rootScope.userName= $scope.user.email;
+						$rootScope.client = new WindowsAzure.MobileServiceClient('https://circleapp.azurewebsites.net').withFilter(function (request, next, callback) {
+               request.headers['x-zumo-auth'] = response.token;
+               next(request, callback);
+            });
+            //change this to go to home page
+            $location.path('/tab/dash');
+        })
+        .error(function(data, status, headers, config) {
+            // handle error things
+            console.log("Error occurred - " + status);
+            // For local testing, you may need to change the ip value to your ip from ipconfig
+            $rootScope.client = new WindowsAzure.MobileServiceClient('http://192.168.1.4:50770');
+        })
+    }
 })
 
-myApp.controller('RequireCtrl', function($scope, $http, $rootScope, $location){
-
+myApp.controller('RequireCtrl', function($scope, $http, $rootScope, $location, registrationService){
+    $scope.newUser = registrationService;
     $scope.goPassword= function() {
         $location.path('/registration_password');
     }
 })
 
-myApp.controller('PasswordCtrl', function($scope, $http, $rootScope, $location){
-
+myApp.controller('PasswordCtrl', function($scope, $http, $rootScope, $location, registrationService){
+    $scope.newUser = registrationService;
     $scope.goOptional= function() {
         $location.path('/registration_optional');
     }
 })
 
 
-myApp.controller('OptionalCtrl', function($scope, $http, $rootScope, $location){
+myApp.controller('OptionalCtrl', function($scope, $http, $rootScope, $location,registrationService){
+    $scope.newUser = registrationService;
     $scope.goImportFriend = function() {
-        $location.path('/tab/dash');
+      console.log("REGSITER user: " + $scope.newUser.email + " - PW: " + $scope.newUser.password +
+      " PHONE NUMBER " + $scope.newUser.phoneNumber);
+      $http({
+          method: 'POST',
+          url: "https://circleapp.azurewebsites.net/tables/User",
+          data: {email: $scope.newUser.email, phoneNumber: $scope.newUser.phoneNumber.replace("+",""),
+          password: $scope.newUser.password, gender: $scope.newUser.gender,
+          name: $scope.newUser.name, age: $scope.newUser.age},
+          headers: {'Content-Type': 'application/json'}
+      })
+      .success(function(response) {
+          // handle success things
+          console.log(response.token);
+          //change this to go to home page
+          $location.path('/tab/dash');
+      })
+      .error(function(data, status, headers, config) {
+          // handle error things
+          console.log("Error occurred - " + status);
+      })
+    }
+
+    $scope.register = function() {
+
     }
 })
